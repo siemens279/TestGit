7ce1f37b94c5a37accc4d5804aafc4194a63b834
siemens279 <siemens279@gmail.com>
2017-06-27 20:48:31 +0300

    0.3 tuy7796utyu

--- app/src/main/java/com/siemens/testgit/MainActivity.java
+++ app/src/main/java/com/siemens/testgit/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("MyLog", "99909");
-
+        Log.d("MyLog", "99909");
         Log.d("MyLog", "99909");
 
     }
